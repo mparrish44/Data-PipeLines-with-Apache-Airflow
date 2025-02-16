3049652fd924021259d9ee0cc63840389d22be6a
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-16 00:16:37 -0800

    Update create_tables.sql

--- airflow1/create_tables.sql
+++ airflow1/create_tables.sql
@@ -1,83 +1,150 @@
-CREATE TABLE public.artists (
-	artistid varchar(256) NOT NULL,
-	name varchar(256),
-	location varchar(256),
-	lattitude numeric(18,0),
-	longitude numeric(18,0)
-);
+class SqlQueries:
+    # Table Drop Queries
+    staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+    staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
+    songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+    user_table_drop = "DROP TABLE IF EXISTS users;"
+    song_table_drop = "DROP TABLE IF EXISTS songs;"
+    artist_table_drop = "DROP TABLE IF EXISTS artists;"
+    time_table_drop = "DROP TABLE IF EXISTS time;"
 
-CREATE TABLE public.songplays (
-	playid varchar(32) NOT NULL,
-	start_time timestamp NOT NULL,
-	userid int4 NOT NULL,
-	"level" varchar(256),
-	songid varchar(256),
-	artistid varchar(256),
-	sessionid int4,
-	location varchar(256),
-	user_agent varchar(256),
-	CONSTRAINT songplays_pkey PRIMARY KEY (playid)
-);
+    # Table Creation Queries
+    staging_events_table_create = """
+        CREATE TABLE IF NOT EXISTS staging_events (
+            artist VARCHAR,
+            auth VARCHAR,
+            firstName VARCHAR,
+            gender CHAR(1),
+            itemInSession INT, 
+            lastName VARCHAR,
+            length FLOAT,
+            level VARCHAR,
+            location VARCHAR,
+            method VARCHAR,
+            page VARCHAR,
+            registration BIGINT,
+            sessionId INT,
+            song VARCHAR,
+            status INT,
+            ts BIGINT,
+            userAgent VARCHAR,
+            userId INT
+        );
+    """
 
-CREATE TABLE public.songs (
-	songid varchar(256) NOT NULL,
-	title varchar(256),
-	artistid varchar(256),
-	"year" int4,
-	duration numeric(18,0),
-	CONSTRAINT songs_pkey PRIMARY KEY (songid)
-);
+    staging_songs_table_create = """
+        CREATE TABLE IF NOT EXISTS staging_songs (
+            num_songs INT,
+            artist_id VARCHAR,
+            artist_latitude FLOAT,
+            artist_longitude FLOAT,
+            artist_location VARCHAR,
+            artist_name VARCHAR,
+            song_id VARCHAR,
+            title VARCHAR,
+            duration FLOAT,
+            year INT
+        );
+    """
 
-CREATE TABLE public.staging_events (
-	artist varchar(256),
-	auth varchar(256),
-	firstname varchar(256),
-	gender varchar(256),
-	iteminsession int4,
-	lastname varchar(256),
-	length numeric(18,0),
-	"level" varchar(256),
-	location varchar(256),
-	"method" varchar(256),
-	page varchar(256),
-	registration numeric(18,0),
-	sessionid int4,
-	song varchar(256),
-	status int4,
-	ts int8,
-	useragent varchar(256),
-	userid int4
-);
+    songplay_table_create = """
+        CREATE TABLE IF NOT EXISTS songplays (
+            songplay_id VARCHAR PRIMARY KEY,
+            start_time TIMESTAMP NOT NULL,
+            user_id INT NOT NULL,
+            level VARCHAR,
+            song_id VARCHAR NOT NULL,
+            artist_id VARCHAR NOT NULL,
+            session_id INT,
+            location VARCHAR,
+            user_agent VARCHAR
+        );
+    """
 
-CREATE TABLE public.staging_songs (
-	num_songs int4,
-	artist_id varchar(256),
-	artist_name varchar(256),
-	artist_latitude numeric(18,0),
-	artist_longitude numeric(18,0),
-	artist_location varchar(256),
-	song_id varchar(256),
-	title varchar(256),
-	duration numeric(18,0),
-	"year" int4
-);
+    user_table_create = """
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INT PRIMARY KEY,
+            first_name VARCHAR,
+            last_name VARCHAR,
+            gender CHAR(1),
+            level VARCHAR NOT NULL
+        );
+    """
 
-CREATE TABLE public."time" (
-	start_time timestamp NOT NULL,
-	"hour" int4,
-	"day" int4,
-	week int4,
-	"month" varchar(256),
-	"year" int4,
-	weekday varchar(256),
-	CONSTRAINT time_pkey PRIMARY KEY (start_time)
-) ;
+    song_table_create = """
+        CREATE TABLE IF NOT EXISTS songs (
+            song_id VARCHAR PRIMARY KEY,
+            title VARCHAR,
+            artist_id VARCHAR NOT NULL,
+            year INT,
+            duration FLOAT
+        );
+    """
 
-CREATE TABLE public.users (
-	userid int4 NOT NULL,
-	first_name varchar(256),
-	last_name varchar(256),
-	gender varchar(256),
-	"level" varchar(256),
-	CONSTRAINT users_pkey PRIMARY KEY (userid)
-);
+    artist_table_create = """
+        CREATE TABLE IF NOT EXISTS artists (
+            artist_id VARCHAR PRIMARY KEY,
+            name VARCHAR,
+            location VARCHAR,
+            latitude FLOAT,
+            longitude FLOAT
+        );
+    """
+
+    time_table_create = """
+        CREATE TABLE IF NOT EXISTS time (
+            start_time TIMESTAMP PRIMARY KEY,
+            hour INT,
+            day INT,
+            week INT,
+            month INT,
+            year INT,
+            weekday INT
+        );
+    """
+
+    #Table Insert Queries
+    
+    songplay_table_insert = """
+        SELECT
+            md5(events.sessionid || events.start_time) songplay_id,
+            events.start_time, 
+            events.userid, 
+            events.level, 
+            songs.song_id, 
+            songs.artist_id, 
+            events.sessionid,   
+            events.location, 
+            events.useragent
+        FROM
+            (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
+            FROM staging_events
+            WHERE page='NextSong') events
+        LEFT JOIN staging_songs songs
+        ON events.song = songs.title
+            AND events.artist = songs.artist_name
+            AND events.length = songs.duration
+            WHERE songs.song_id IS NOT NULL;
+    """
+
+    user_table_insert = """
+        SELECT DISTINCT userid, firstname, lastname, gender, level
+        FROM staging_events
+        WHERE page='NextSong'
+    """
+
+    song_table_insert = """
+        SELECT DISTINCT song_id, title, artist_id, year, duration
+        FROM staging_songs
+    """
+
+    artist_table_insert = """
+        SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+        FROM staging_songs
+    """
+
+    time_table_insert = """
+        SELECT start_time, EXTRACT(hour FROM start_time), EXTRACT(day FROM start_time), EXTRACT(week FROM start_time),
+               EXTRACT(month FROM start_time), EXTRACT(year FROM start_time), EXTRACT(dayofweek FROM start_time)
+        FROM songplays
+    """
