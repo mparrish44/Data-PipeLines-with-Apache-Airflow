fc0bcb3e9a87b9da173da2e38b49058cbbee1eb3
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-16 00:04:30 -0800

    Update load_dimension.py

--- airflow1/plugins/operators/load_dimension.py
+++ airflow1/plugins/operators/load_dimension.py
@@ -1,22 +1,42 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class LoadDimensionOperator(BaseOperator):
-
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id='',
+                 table_name='',
+                 select_sql='',
+                 append_mode=True,
                  *args, **kwargs):
-
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table_name = table_name
+        self.select_sql = select_sql
+        self.append_mode = append_mode
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f'Loading data into {self.table_name}')
+
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.append_mode:
+            insert_sql = f"""
+                INSERT INTO {self.table_name}
+                {self.select_sql}
+                """
+        else:
+            truncate_sql = f"""
+                TRUNCATE TABLE {self.table_name};
+                """
+            insert_sql = f"""
+                {truncate_sql}
+                {self.select_sql}
+                """
+
+        self.log.info(f"Running SQL command: {insert_sql}")
+        redshift_hook.run(insert_sql)
