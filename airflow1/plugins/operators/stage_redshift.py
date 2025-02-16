95103225d402a778f4fea1b27a0cdcf7c0e538b3
mparrish44 <120215229+mparrish44@users.noreply.github.com>
2025-02-16 00:05:24 -0800

    Update stage_redshift.py

--- airflow1/plugins/operators/stage_redshift.py
+++ airflow1/plugins/operators/stage_redshift.py
@@ -1,26 +1,51 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
-from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook  # Correct Import
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
-
-    @apply_defaults
+                                      
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id='',
+                 aws_credentials_id='',
+                 table_name='',
+                 s3_bucket='',
+                 s3_key='',
+                 json_path='',
                  *args, **kwargs):
-
-        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        super().__init__(*args, **kwargs)  # No need for class name here
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table_name = table_name
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        execution_date = context['data_interval_start']  # Extract execution date
+        formatted_execution_date = execution_date.strftime('%Y-%m-%d')  # Format properly
 
+        self.log.info(f'Staging {self.table_name} from S3 to Redshift')
 
+        aws_hook = AwsBaseHook(aws_conn_id=self.aws_credentials_id, client_type='s3')
+        credentials = aws_hook.get_credentials()
 
+        # Ensure s3_key is formatted before constructing SQL query
+        s3_key_with_timestamp = self.s3_key.format(formatted_execution_date)
+        s3_path = f's3://{self.s3_bucket}/{s3_key_with_timestamp}'
 
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        # Construct SQL query with properly formatted values
+        copy_sql = f"""
+        COPY {self.table_name}
+        FROM '{s3_path}'
+        ACCESS_KEY_ID '{credentials.access_key}'
+        SECRET_ACCESS_KEY '{credentials.secret_key}'
+        JSON '{self.json_path}'
+        TIMEFORMAT 'auto';
+        """
+
+        self.log.info(f"Running COPY command: {copy_sql}")
+        redshift_hook.run(copy_sql)
